0365b6152246c44a72e5ef0497e5c364f543135f
suyunkai <82149368+suyunkai@users.noreply.github.com>
2023-08-19 12:23:18 +0800

    Update model.ts

--- src/components/common/Setting/model.ts
+++ src/components/common/Setting/model.ts
@@ -1,66 +1,5 @@
-export class ConfigState {
-  timeoutMs?: number
-  apiKey?: string
-  accessToken?: string
-  accessTokenExpiredTime?: string
-  apiBaseUrl?: string
-  apiModel?: APIMODEL
-  reverseProxy?: string
-  socksProxy?: string
-  socksAuth?: string
-  httpsProxy?: string
-  balance?: number
-  siteConfig?: SiteConfig
-  mailConfig?: MailConfig
-  auditConfig?: AuditConfig
-}
-
-// https://platform.openai.com/docs/models/overview
-export type CHATMODEL = 'gpt-3.5-turbo' | 'gpt-3.5-turbo-0301' | 'gpt-3.5-turbo-0613' | 'gpt-3.5-turbo-16k' | 'gpt-3.5-turbo-16k-0613' | 'gpt-4' | 'gpt-4-0314' | 'gpt-4-32k' | 'gpt-4-32k-0314' | 'gpt-4-0613' | 'gpt-4-32k-0613' | 'text-davinci-002-render-sha-mobile' | 'text-embedding-ada-002' | 'gpt-4-mobile' | 'gpt-4-browsing'
-
-export class UserConfig {
-  chatModel?: CHATMODEL
-}
-
-export class SiteConfig {
-  siteTitle?: string
-  loginEnabled?: boolean
-  loginSalt?: string
-  registerEnabled?: boolean
-  registerReview?: boolean
-  registerMails?: string
-  siteDomain?: string
-}
-
-export class MailConfig {
-  smtpHost?: string
-  smtpPort?: number
-  smtpTsl?: boolean
-  smtpUserName?: string
-  smtpPassword?: string
-}
-export type TextAuditServiceProvider = 'baidu' //  | 'ali'
-
-export interface TextAuditServiceOptions {
-  apiKey: string
-  apiSecret: string
-  label?: string
-}
-export enum TextAudioType {
-  None = 0,
-  Request = 1 << 0, // 二进制 01
-  Response = 1 << 1, // 二进制 10
-  All = Request | Response, // 二进制 11
-}
-
-export class AuditConfig {
-  enabled?: boolean
-  provider?: TextAuditServiceProvider
-  options?: TextAuditServiceOptions
-  textType?: TextAudioType
-  customizeEnabled?: boolean
-  sensitiveWords?: string
-}
+import type { ObjectId } from 'mongodb'
+import type { TextAuditServiceOptions, TextAuditServiceProvider } from 'src/utils/textAudit'
 
 export enum Status {
   Normal = 0,
@@ -84,8 +23,211 @@ export enum UserRole {
   Partner = 8,
 }
 
+export class UserInfo {
+  _id: ObjectId
+  name: string
+  email: string
+  password: string
+  status: Status
+  createTime: string
+  verifyTime?: string
+  avatar?: string
+  description?: string
+  updateTime?: string
+  config?: UserConfig
+  roles?: UserRole[]
+  constructor(email: string, password: string) {
+    this.name = email
+    this.email = email
+    this.password = password
+    this.status = Status.PreVerify
+    this.createTime = new Date().toLocaleString()
+    this.verifyTime = null
+    this.updateTime = new Date().toLocaleString()
+    this.roles = [UserRole.User]
+  }
+}
+
+export class UserConfig {
+  chatModel: CHATMODEL
+}
+
+// https://platform.openai.com/docs/models/overview
+// 除此之外，gpt-4-0314、gpt-4-32k-0314、gpt-3.5-turbo-0301 模型将在 9 月 13 日被弃用。
+export type CHATMODEL = 'gpt-3.5-turbo' | 'gpt-3.5-turbo-0613' | 'gpt-3.5-turbo-16k' | 'gpt-3.5-turbo-16k-0613' | 'net-gpt-3.5-turbo-16k' | 'gpt-4' | 'gpt-4-2k' | 'gpt-4-4k' | 'gpt-4-32k' | 'gpt-4-0613' | 'gpt-4-32k-0613' | 'net-gpt-4' | 'gpt-4-mobile' | 'gpt-4-browsing' | 'claude-2-100k' | 'google-palm' | 'llama-2-70b' | 'mj'
+
+export const CHATMODELS: CHATMODEL[] = [
+  'gpt-3.5-turbo', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-16k-0613', 'net-gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-2k', 'gpt-4-4k', 'gpt-4-32k', 'gpt-4-0613', 'gpt-4-32k-0613', 'net-gpt-4', 'gpt-4-mobile', 'gpt-4-browsing', 'claude-2-100k', 'google-palm', 'llama-2-70b', 'mj',
+]
+
+export const chatModelOptions = [
+  'gpt-3.5-turbo', 'gpt-3.5-turbo-0613', 'gpt-3.5-turbo-16k', 'gpt-3.5-turbo-16k-0613', 'net-gpt-3.5-turbo-16k', 'gpt-4', 'gpt-4-2k', 'gpt-4-4k', 'gpt-4-32k', 'gpt-4-0613', 'gpt-4-32k-0613', 'net-gpt-4', 'gpt-4-mobile', 'gpt-4-browsing', 'claude-2-100k', 'google-palm', 'llama-2-70b', 'mj',
+].map((model: string) => {
+  let label = model
+  if (model === 'text-davinci-002-render-sha-mobile')
+    label = 'gpt-3.5-mobile'
+  return {
+    label,
+    key: model,
+    value: model,
+  }
+})
+
+export class ChatRoom {
+  _id: ObjectId
+  roomId: number
+  userId: string
+  title: string
+  prompt: string
+  usingContext: boolean
+  status: Status = Status.Normal
+  // only access token used
+  accountId?: string
+  chatModel: CHATMODEL
+  constructor(userId: string, title: string, roomId: number) {
+    this.userId = userId
+    this.title = title
+    this.prompt = undefined
+    this.roomId = roomId
+    this.usingContext = true
+    this.accountId = null
+    this.chatModel = null
+  }
+}
+
+export class ChatOptions {
+  parentMessageId?: string
+  messageId?: string
+  conversationId?: string
+  prompt_tokens?: number
+  completion_tokens?: number
+  total_tokens?: number
+  estimated?: boolean
+  constructor(parentMessageId?: string, messageId?: string, conversationId?: string) {
+    this.parentMessageId = parentMessageId
+    this.messageId = messageId
+    this.conversationId = conversationId
+  }
+}
+
+export class previousResponse {
+  response: string
+  options: ChatOptions
+}
+
+export class ChatInfo {
+  _id: ObjectId
+  roomId: number
+  uuid: number
+  dateTime: number
+  prompt: string
+  response?: string
+  status: Status = Status.Normal
+  options: ChatOptions
+  previousResponse?: previousResponse[]
+  constructor(roomId: number, uuid: number, prompt: string, options: ChatOptions) {
+    this.roomId = roomId
+    this.uuid = uuid
+    this.prompt = prompt
+    this.options = options
+    this.dateTime = new Date().getTime()
+  }
+}
+
+export class UsageResponse {
+  prompt_tokens: number
+  completion_tokens: number
+  total_tokens: number
+  estimated: boolean
+}
+
+export class ChatUsage {
+  _id: ObjectId
+  userId: ObjectId
+  roomId: number
+  chatId: ObjectId
+  messageId: string
+  promptTokens: number
+  completionTokens: number
+  totalTokens: number
+  estimated: boolean
+  dateTime: number
+  constructor(userId: ObjectId, roomId: number, chatId: ObjectId, messageId: string, usage: UsageResponse) {
+    this.userId = userId
+    this.roomId = roomId
+    this.chatId = chatId
+    this.messageId = messageId
+    if (usage) {
+      this.promptTokens = usage.prompt_tokens
+      this.completionTokens = usage.completion_tokens
+      this.totalTokens = usage.total_tokens
+      this.estimated = usage.estimated
+    }
+    this.dateTime = new Date().getTime()
+  }
+}
+
+export class Config {
+  constructor(
+    public _id: ObjectId,
+    public timeoutMs: number,
+    public apiKey?: string,
+    public apiDisableDebug?: boolean,
+    public accessToken?: string,
+    public apiBaseUrl?: string,
+    public apiModel?: APIMODEL,
+    public reverseProxy?: string,
+    public socksProxy?: string,
+    public socksAuth?: string,
+    public httpsProxy?: string,
+    public siteConfig?: SiteConfig,
+    public mailConfig?: MailConfig,
+    public auditConfig?: AuditConfig,
+  ) { }
+}
+
+export class SiteConfig {
+  constructor(
+    public siteTitle?: string,
+    public loginEnabled?: boolean,
+    public loginSalt?: string,
+    public registerEnabled?: boolean,
+    public registerReview?: boolean,
+    public registerMails?: string,
+    public siteDomain?: string,
+  ) { }
+}
+
+export class MailConfig {
+  constructor(
+    public smtpHost: string,
+    public smtpPort: number,
+    public smtpTsl: boolean,
+    public smtpUserName: string,
+    public smtpPassword: string,
+  ) { }
+}
+
+export class AuditConfig {
+  constructor(
+    public enabled: boolean,
+    public provider: TextAuditServiceProvider,
+    public options: TextAuditServiceOptions,
+    public textType: TextAudioType,
+    public customizeEnabled: boolean,
+    public sensitiveWords: string,
+  ) { }
+}
+
+export enum TextAudioType {
+  None = 0,
+  Request = 1 << 0, // 二进制 01
+  Response = 1 << 1, // 二进制 10
+  All = Request | Response, // 二进制 11
+}
+
 export class KeyConfig {
-  _id?: string
+  _id: ObjectId
   key: string
   keyModel: APIMODEL
   chatModels: CHATMODEL[]
@@ -102,30 +244,4 @@ export class KeyConfig {
   }
 }
 
-export type APIMODEL = 'ChatGPTAPI' | 'ChatGPTUnofficialProxyAPI' | undefined
-
-export const apiModelOptions = ['ChatGPTAPI', 'ChatGPTUnofficialProxyAPI'].map((model: string) => {
-  return {
-    label: model,
-    key: model,
-    value: model,
-  }
-})
-
-export const userRoleOptions = Object.values(UserRole).filter(d => isNaN(Number(d))).map((role) => {
-  return {
-    label: role as string,
-    key: role as string,
-    value: UserRole[role as keyof typeof UserRole],
-  }
-})
-
-export class UserInfo {
-  _id?: string
-  email?: string
-  password?: string
-  roles: UserRole[]
-  constructor(roles: UserRole[]) {
-    this.roles = roles
-  }
-}
+export type APIMODEL = 'ChatGPTAPI' | 'ChatGPTUnofficialProxyAPI'
