44b3acdbfce9256d82177666435ffdd9ad877923
suyunkai <82149368+suyunkai@users.noreply.github.com>
2023-08-31 23:04:44 +0800

    Update helper.ts

--- src/store/modules/settings/helper.ts
+++ src/store/modules/settings/helper.ts
@@ -1,8 +1,10 @@
 import { ss } from '@/utils/storage'
 
 const LOCAL_NAME = 'settingsStorage'
+const CURRENT_VERSION = 1; // Update this whenever you change the default settings
 
 export interface SettingsState {
+  version: number
   systemMessage: string
   temperature: number
   top_p: number
@@ -10,6 +12,7 @@ export interface SettingsState {
 
 export function defaultSetting(): SettingsState {
   return {
+    version: CURRENT_VERSION,
     systemMessage: '',
     temperature: 0.8,
     top_p: 1,
@@ -18,7 +21,16 @@ export function defaultSetting(): SettingsState {
 
 export function getLocalState(): SettingsState {
   const localSetting: SettingsState | undefined = ss.get(LOCAL_NAME)
-  return { ...defaultSetting(), ...localSetting }
+  const defaultSettings = defaultSetting();
+  
+  if (!localSetting || localSetting.version !== defaultSettings.version) {
+    // If there is no local setting, or the version of the local setting is not the current version,
+    // overwrite the local setting with the default settings
+    setLocalState(defaultSettings);
+    return defaultSettings;
+  }
+  
+  return { ...defaultSettings, ...localSetting }
 }
 
 export function setLocalState(setting: SettingsState): void {
